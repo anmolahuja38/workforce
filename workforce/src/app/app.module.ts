42aa3984d2e1c3ab9ffa96db50469258bb0fbdb8
anmolahuja38 <70136141+anmolahuja38@users.noreply.github.com>
2020-12-11 23:01:26 +0530

    Created ListEmployeeComponent and PageNotFoundComponent with Routing

--- workforce/src/app/app.module.ts
+++ workforce/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: 'employee', component: ListEmployeeComponent },
+  { path: '', redirectTo: '/employee', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmployeeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
