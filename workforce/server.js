bdca70f5585928645a1e715f79e587c22291f4ed
anmolahuja38 <70136141+anmolahuja38@users.noreply.github.com>
2021-01-05 15:00:37 +0530

    Update server.js

--- workforce/server.js
+++ workforce/server.js
@@ -5,11 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/workforce'));
+app.use(express.static('./dist/workforce'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/workforce/index.html'));
+    res.sendFile('index.html', {root: 'dist/workforce/'}
+    );
 });
 
 // Start the app by listening on the default Heroku port
